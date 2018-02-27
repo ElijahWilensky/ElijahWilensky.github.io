e7fe603138f166e9b2e20d53812796e677896330
ElijahWilensky <36679808+ElijahWilensky@users.noreply.github.com>
2018-02-27 14:22:07 -0800

    Update smiley.js

--- smiley.js
+++ smiley.js
@@ -1,10 +1,52 @@
-"!./processing.js"
-fill(255, 234, 0);
-    ellipse(faceObj.mouseX, faceObj.mouseY, 150, 150);
-    fill(0, 0, 0);
-    ellipse(faceObj.mouseX-30, faceObj.mouseY-30, 20, 20); 
-    ellipse(faceObj.mouseX+30, faceObj.mouseY-30, 20, 20); 
-    noFill(); 
-    strokeWeight(3);
-    arc(faceObj.mouseX, faceObj.centerY+10, 64, 40, 0, 180);
-};
+<!DOCTYPE html>
+<html>
+<head>
+<style> 
+#face {
+	position: absolute;
+    left:10px;
+    top: 300px;
+    height: 50px;
+    -webkit-transition: width 2s; /* For Safari 3.1 to 6.0 */
+    transition: width 2s, left 2s, top 2s;
+
+}
+
+#face:hover {
+    left: 100px;
+    top: 0px;
+}
+</style>
+</head>
+<body>
+
+  <img src= "https://images-na.ssl-images-amazon.com/images/I/51zLZbEVSTL._SY355_.jpg" id="face">
+  <script>
+  fill(0,0,0);
+  rect(50,50,100,100);
+   var Player = function(x,y) {
+   this.x = 50;
+   this.y = 50;
+  };
+  fill(0, 0, 0);
+  rect(50,50,100,100);
+  Player.prototype.draw = function() {
+    fill(0,0,0);
+    rect(50,50,100,100);  
+  };
+  
+  
+  </script>
+
+  </body>
+  </html>
+
+
+
+
+
+
+
+
+
+
